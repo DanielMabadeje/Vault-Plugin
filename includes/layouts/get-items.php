dd3e2cdd8be90141954c747ccf87f108ec394453
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-08-18 22:18:17 -0700

    edited get-items layout

--- includes/layouts/get-items.php
+++ includes/layouts/get-items.php
@@ -39,7 +39,54 @@
         )
     );
 
+    if ($items->have_posts()) {
+    ?>
+        <?php
+        while ($items->have_posts()) {
 
+
+        ?>
+            <section>
+                <div class="container d-flex justify-content-center">
+                    <figure class="card card-product-grid card-lg"> <a href="#" class="img-wrap" data-abc="true"> <img src="https://i.imgur.com/MPqUt62.jpg"> </a>
+                        <figcaption class="info-wrap">
+                            <div class="row">
+                                <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">Dell Xtreme 270</a> <span class="rated">Laptops</span> </div>
+                                <div class="col-md-3 col-xs-3">
+                                    <div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <span class="rated">Rated 4.0/5</span> </div>
+                                </div>
+                            </div>
+                        </figcaption>
+                        <div class="bottom-wrap-payment">
+                            <figcaption class="info-wrap">
+                                <div class="row">
+                                    <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">$3,999</a> <span class="rated">VISA Platinum</span> </div>
+                                    <div class="col-md-3 col-xs-3">
+                                        <div class="rating text-right"> #### 8787 </div>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </div>
+                        <div class="bottom-wrap"> <a href="#" class="btn btn-primary float-right" data-abc="true"> Buy now </a>
+                            <div class="price-wrap"> <a href="#" class="btn btn-warning float-left" data-abc="true"> Cancel </a> </div>
+                        </div>
+                    </figure>
+                </div>
+            </section>
+
+        <?php };
+    } else {
+        ?>
+        <section>
+            <div class="card p-5 border-none">
+                <div class="card-body">
+                    <h3 class="display-3">You currently do not have any Item in your vault</h3>
+                </div>
+            </div>
+
+        </section>
+    <?php
+    }
     ?>
 
 </body>
