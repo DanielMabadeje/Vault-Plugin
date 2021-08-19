b8e078d28d6f23c2571c048b4ec759759b6f3617
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-08-18 22:28:31 -0700

    edited user registration

--- includes/class-user-registration.php
+++ includes/class-user-registration.php
@@ -2,9 +2,12 @@
 
 class UserRegistration
 {
+
+    private $uniqueSKI;
     public function __construct()
     {
         add_shortcode('vault_user_registration', array($this, 'showRegistrationForm'));
+        $this->uniqueSKI = $uniqueSKI;
     }
 
     public function showRegistrationForm()
@@ -24,6 +27,31 @@ class UserRegistration
     }
     public function generateSKI()
     {
-        # code...
+        $this->uniqueSKI = $this->generateUniqueIdAndSki();
+
+        if ($this->validateSKI($this->uniqueSKI)) {
+            $this->uniqueSKI = $this->generateUniqueIdAndSki();
+        } else {
+            # code...
+        }
+
+        return $this->uniqueSKI;
+    }
+
+    public function generateUniqueIdAndSki(Type $var = null)
+    {
+        $uniqueId = uniqid();
+        return $uniqueId;
+    }
+
+    public function validateSKI($ski)
+    {
+
+
+        if (condition) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
