00ea9cc40cd3e8eb16bbeadfe3b76554fe6ea027
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-08-18 22:19:16 -0700

    edited get-items layout

--- includes/layouts/get-items.php
+++ includes/layouts/get-items.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
 <body>
 
     <?php if (!empty($pageno) or $_GET['pageno'] > 1) {
@@ -90,5 +80,3 @@
     ?>
 
 </body>
-
-</html>
