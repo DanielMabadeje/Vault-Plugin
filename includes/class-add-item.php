a037bb37e1b58ba7a6d94ad2616a6b9cef62b1e0
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-08-19 23:39:47 -0700

    Added files

---
+++ includes/class-add-item.php
@@ -0,0 +1,14 @@
+<?php
+class AddItem
+{
+
+    public function __construct()
+    {
+        # code...
+    }
+
+    public function addItem(array $data)
+    {
+        wp_insert_post( array $data );
+    }
+}
