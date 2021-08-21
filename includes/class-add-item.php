f06df543e4d7eacadfeed57159a4934cf9bb1043
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-08-21 12:19:22 -0700

    fixed bug

--- includes/class-add-item.php
+++ includes/class-add-item.php
@@ -7,6 +7,21 @@ class AddItem
         # code...
     }
 
+
+    public function addItemForm()
+    {
+        ob_start();
+
+        // include plugin_dir_path( dirname( __FILE__ ) ) . 'public/layouts/design-transfer-wallet-to-wallet.php';
+        include plugin_dir_path(__FILE__) . 'layouts/user-registration.php';
+         
+        $output = ob_get_clean();
+  
+        return $output;
+      
+    }
+
+
     public function addItem(array $data)
     {
         // wp_insert_post( array $data);
